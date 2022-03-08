d7f2d27b849257e3b4c6e58f5dd55d483d980636
Kussa Laksana Utama <kussa.laksana@gmail.com>
2022-03-08 11:12:48 +0800

    second revision

---
+++ LDA/lda.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from nlpia.data.loaders import get_data
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize.casual import casual_tokenize
+from sklearn.preprocessing import MinMaxScaler
+from pugnlp.stats import Confusion
+pd.options.display.width = 120
+
+Class LDA:
+
+def __init__(self)
+def dataProcessor(s):
+    index = ['sms-{i}{j}'.format(i=y, j='spam' if x==1 else 'notspam')for y,x in enumerate(sms.spam)]
+    sms = pd.DataFrame(sms.values, columns = sms.columns, index = index)
+    sms['spam']=sms.spam.astype(int)
+    return sms
