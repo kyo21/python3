df9c33945c3547f16fb56e291ae60ee2ca188dfb
Kussa Laksana Utama <kussa.laksana@gmail.com>
2022-03-08 10:25:08 +0800

    first commit

---
+++ test2.py
@@ -0,0 +1,33 @@
+import numpy as np
+# z = np.zeros([3,4])
+# print(z)
+
+# class Mahasiswa:
+#     def __init__ (self):
+#         self.nama = None
+#         self.NIM = None
+#     def setNama(self, nama):
+#         self.nama = nama
+#     def setNIM(self, NIM):
+#         self.NIM = NIM
+
+# mahasiswa = Mahasiswa()
+# mahasiswa.setNama("Andri")
+# print(mahasiswa.nama)
+
+# a = [1,0,1]
+# b = [['ayam', 'burung','angsa'],['anjing','babi','ular'],['sapi','kambing','kuda']]
+# zip_list = zip(a,b)
+# for i,j in zip_list:
+#     print(i)
+
+x = [1., 0., 1., 1.]
+w = [.3,.2,1.,.5]
+x_vect, w_vect = np.array(x), np.array(w)
+
+z = (x_vect.transpose().dot(w_vect))
+y = (x_vect.dot(w_vect))
+print(z, ' ', y)
+
+def entropy_loss():
+    
